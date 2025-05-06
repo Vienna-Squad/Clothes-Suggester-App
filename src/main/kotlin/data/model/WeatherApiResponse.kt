b374bff3656d3d713aa6975a7a4e3e29a66cf3d8
Mostafa Ibrahim <mostafa1996998@gmail.com>
2025-05-06 15:33:10 +0300

    refactor WeatherApiResponse to make serial name

--- src/main/kotlin/data/model/WeatherApiResponse.kt
+++ src/main/kotlin/data/model/WeatherApiResponse.kt
@@ -1,36 +1,46 @@
 package org.example.data.model
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
-class WeatherApiResponse {
-    @Serializable
-    data class WeatherApiResponse(
-        val location: Location,
-        val current: CurrentWeather
-    )
+@Serializable
+data class WeatherApiResponse(
+    val location: Location,
+    val current: CurrentWeather
+)
 
-    @Serializable
-    data class Location(
-        val name: String,
-        val region: String,
-        val country: String,
-        val lat: Double,
-        val lon: Double,
-        val localtime:String
-    )
+@Serializable
+data class Location(
+    val name: String,
+    val region: String,
+    val country: String,
+    @SerialName("lat")
+    val latitude: Double,
+    @SerialName("lon")
+    val longitude: Double,
+    val localtime: String
+)
 
-    @Serializable
-    data class CurrentWeather(
-        val temp_c: Double,
-        val precip_mm: Double,
-        val humidity: Int,
-        val wind_kph: Double,
-        val condition: Condition
-    )
+@Serializable
+data class CurrentWeather(
+    val temp_c: Double,
 
-    @Serializable
-    data class Condition(
-        val text: String,
-        val icon: String
-    )
-}
+    @SerialName("precip_mm")
+    val precipitationMm: Double,
+
+    val humidity: Int,
+
+    @SerialName("wind_kph")
+    val windSpeedKph: Double,
+
+    val condition: Condition
+)
+
+@Serializable
+data class Condition(
+    @SerialName("text")
+    val description: String,
+
+    @SerialName("icon")
+    val iconUrl: String
+)
