3ca93a572178752e3b0af6aeea1a297891b3f3bd
Mostafa Ibrahim <mostafa1996998@gmail.com>
2025-05-06 15:43:55 +0300

    implement logic for WeatherRepositoryImpl to pass tests

--- src/main/kotlin/data/repository/WeatherRepositoryImpl.kt
+++ src/main/kotlin/data/repository/WeatherRepositoryImpl.kt
@@ -1,6 +1,5 @@
 package org.example.data.repository
 
-import org.example.data.model.WeatherApiResponse
 import org.example.data.remote.WeatherService
 import org.example.domain.InvalidCityException
 import org.example.domain.entity.Weather
@@ -9,7 +8,19 @@ import org.example.domain.repository.WeatherRepository
 class WeatherRepositoryImpl(
     private val weatherService: WeatherService
 ):WeatherRepository {
-    override suspend fun getWeather(city: String): Result<Weather> {
-        TODO("Not yet implemented")
-}
-}
+            override suspend fun getWeather(city: String): Result<Weather> {
+                return weatherService.getWeather(city).map { response ->
+                    val cityName = response.location.name ?: throw InvalidCityException("City name is missing in API response")
+                    Weather(
+                        city = cityName,
+                        country = response.location.country,
+                        time = response.location.localtime,
+                        temperature = response.current.temp_c,
+                        description = response.current.condition.description                    )
+                }
+            }
+
+
+        }
+
+
