2e6bdd8573eb3505d49bc4491ffd1f0218e6cf70
Mostafa Ibrahim <mostafa1996998@gmail.com>
2025-05-06 15:30:45 +0300

    add WeatherAppException

--- src/main/kotlin/domain/Exception.kt
+++ src/main/kotlin/domain/Exception.kt
@@ -1,2 +1,7 @@
 package org.example.domain
+abstract class WeatherAppException(message: String) : Exception(message)
+
+class WeatherApiException(message: String) : WeatherAppException(message)
+class InvalidCityException(message: String) : WeatherAppException(message)
+class CityNotFoundException(message: String) : WeatherAppException(message)
 
