ce73c8110953d2dc00990a2ac5bc2b27aab8fa27
nada <nadamansoursamir@gmail.com>
2025-05-06 15:30:34 +0300

    add GetCurrentWeatherUseCase implementation and GetCurrentWeatherUiController

--- src/main/kotlin/domain/usecase/GetCurrentWeatherUseCase.kt
+++ src/main/kotlin/domain/usecase/GetCurrentWeatherUseCase.kt
@@ -3,11 +3,11 @@ package org.example.domain.usecase
 import org.example.domain.entity.Weather
 import org.example.domain.repository.WeatherRepository
 
-class GetCurrentWeatherUseCase (
-    private val weatherRepository:WeatherRepository
-){
+class GetCurrentWeatherUseCase(
+    private val weatherRepository: WeatherRepository
+) {
 
-    suspend operator fun invoke(city:String):Result<Weather>{
-        return TODO()
+    suspend operator fun invoke(city: String): Result<Weather> {
+        return weatherRepository.getWeather(city)
     }
 }
