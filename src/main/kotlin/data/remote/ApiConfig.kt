d8df29a08f97d800aabf35d016db5b5c2d933853
Mostafa Ibrahim <mostafa1996998@gmail.com>
2025-05-07 17:54:46 +0300

    remove api key from ApiConfig and put it in local properties

--- src/main/kotlin/data/remote/ApiConfig.kt
+++ src/main/kotlin/data/remote/ApiConfig.kt
@@ -1,7 +1,14 @@
 package org.example.data.remote
 
+import java.io.FileInputStream
+import java.util.*
+
 object ApiConfig {
     const val BASE_URL = "http://api.weatherapi.com/v1"
     const val CURRENT_WEATHER_ENDPOINT = "/current.json"
-    const val API_KEY = "a992774d75ec49dfb08114556250505"
+    val API_KEY: String by lazy {
+        val properties = Properties()
+        properties.load(FileInputStream("local.properties"))
+        properties.getProperty("api.key") ?: throw IllegalStateException("API key not found in local.properties")
+    }
 }
