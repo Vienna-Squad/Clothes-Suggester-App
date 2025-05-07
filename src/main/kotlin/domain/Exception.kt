2140a90d3cd86063ba721b7a090f2d2b71c300db
Mostafa Ibrahim <mostafa1996998@gmail.com>
2025-05-07 05:26:45 +0300

    add custom exception to use it in test and mapper weather

--- src/main/kotlin/domain/Exception.kt
+++ src/main/kotlin/domain/Exception.kt
@@ -3,5 +3,7 @@ abstract class WeatherAppException(message: String) : Exception(message)
 
 class WeatherApiException(message: String) : WeatherAppException(message)
 class InvalidCityException(message: String) : WeatherAppException(message)
-class CityNotFoundException(message: String) : WeatherAppException(message)
-
+class InvalidCountryException(message: String) : WeatherAppException(message)
+class InvalidLocalTimeException(message: String) : WeatherAppException(message)
+class InvalidTemperatureException(message: String) : WeatherAppException(message)
+class InvalidDescriptionException(message: String) : WeatherAppException(message)
