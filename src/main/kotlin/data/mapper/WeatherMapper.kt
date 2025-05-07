829d0e695a2c096aca3aa63d22d83f75d43cbe4f
Mostafa Ibrahim <mostafa1996998@gmail.com>
2025-05-07 05:32:27 +0300

    implement logic for WeatherMapper to pass tests

--- src/main/kotlin/data/mapper/WeatherMapper.kt
+++ src/main/kotlin/data/mapper/WeatherMapper.kt
@@ -1,12 +1,24 @@
 package org.example.data.mapper
 
 import org.example.data.model.WeatherApiResponse
+import org.example.domain.*
 import org.example.domain.entity.Weather
 
 class WeatherMapper {
     fun mapDtoToWeather(weatherApiResponse: WeatherApiResponse): Weather {
-
-        TODO("")
-
+        return Weather(
+            city = weatherApiResponse.location.name
+                ?: throw InvalidCityException("City name is missing in API response"),
+            country = weatherApiResponse.location.country
+                ?: throw InvalidCountryException("country name is missing in API response"),
+            time = weatherApiResponse.location.localtime
+                ?: throw InvalidLocalTimeException("time is missing in API response"),
+            temperature = weatherApiResponse.current.temp_c
+                ?: throw InvalidTemperatureException("temperature is missing in API response"),
+            description = weatherApiResponse.current.condition.description
+                ?: throw InvalidDescriptionException("description is missing in API response")
+        )
     }
+
+
 }
